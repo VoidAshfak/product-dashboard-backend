570113ae1f94f73c35ab11fe1b49c2cf565f31f3
VoidAshfak <asfaqul.asif@gmail.com>
2025-11-16 12:41:27 +0600

    changed middleware response

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,7 +5,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/login").post(loginUser);
-router.route("/me").get(verifyJWT, refresh);
+router.route("/me").get(refresh);
 router.route("/logout").post(verifyJWT, logout);
 
 export default router;
